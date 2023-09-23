b40966d6123fb93270385c59b6074ccf50150914
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2023-09-23 15:37:12 +1000

    Getting closer

--- test/mock_channels_test.dart
+++ test/mock_channels_test.dart
@@ -4,19 +4,23 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('Receives events from the event channel', (tester) async {
+    var receivedStartMonitoring = false;
+
     final plugin = LocationWakeup();
 
     const eventChannel = EventChannel('loc_stream');
+    final fakeStreamHandler = FakeStreamHandler();
     tester.binding.defaultBinaryMessenger.setMockStreamHandler(
       eventChannel,
-      FakeStreamHandler(),
+      fakeStreamHandler,
     );
 
     tester.binding.defaultBinaryMessenger.setMockMethodCallHandler(
       const MethodChannel('loc'),
       (methodCall) async {
         if (methodCall.method == 'startMonitoring') {
-          return null; // Mock response for startMonitoring
+          receivedStartMonitoring = true;
+          return null;
         }
         throw PlatformException(
           code: 'UNAVAILABLE',
@@ -29,11 +33,28 @@ void main() {
 
     await plugin.startMonitoring();
 
+    final locationData = <String, dynamic>{
+      'latitude': 40.7128,
+      'longitude': -74.0060,
+      'altitude': 500.0,
+      'speed': 5.0,
+      'timestamp': 1677648652,
+      'permissionStatus': 'granted',
+    };
+
+    final methodCall = MethodCall('listen', locationData);
+
+    final encodedData =
+        const StandardMethodCodec().encodeMethodCall(methodCall);
+
+    await eventChannel.binaryMessenger.send('loc_stream', encodedData);
     final events = await plugin.locationUpdates.toList();
     expect(
       events,
       isNotEmpty,
     );
+
+    expect(receivedStartMonitoring, isTrue);
   });
 }
 
