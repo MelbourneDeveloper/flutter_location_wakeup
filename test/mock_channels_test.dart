a7c3101d8d0d016364c2df67f6c9513c3dfbfe31
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2023-09-24 07:37:50 +1000

    Cleanup

--- test/mock_channels_test.dart
+++ test/mock_channels_test.dart
@@ -4,52 +4,52 @@ import 'package:flutter_test/flutter_test.dart';
 import 'test_extensions.dart';
 
 void main() {
-  testWidgets('Receives events from the event channel', (tester) async {
-    var receivedStartMonitoring = false;
+testWidgets('Receives events from the event channel', (tester) async {
+  var receivedStartMonitoring = false;
 
-    //Get the method/event channels
-    final methodChannelLocationWakeup =
-        LocationWakeupPlatform.instance as MethodChannelLocationWakeup;
+  //Get the method/event channels
+  final methodChannelLocationWakeup =
+      LocationWakeupPlatform.instance as MethodChannelLocationWakeup;
 
-    //Create a sender that sends fake events to the EventChannel that mimic
-    //the events that the device platform sends over the EventChannel
-    final sendToEventChannel =
-        tester.getEventChannelSender<LocationWakeup, LocationResult>(
-      methodChannel: methodChannelLocationWakeup.channel,
-      eventChannel: methodChannelLocationWakeup.eventChannel,
-      methodHandler: (methodCall) async {
-        if (methodCall.method == 'startMonitoring') {
-          receivedStartMonitoring = true;
-        }
-        return null;
-      },
-    );
+  //Create a sender that sends fake events to the EventChannel that mimic
+  //the events that the device platform sends over the EventChannel
+  final sendToEventChannel =
+      tester.getEventChannelSender<LocationWakeup, LocationResult>(
+    methodChannel: methodChannelLocationWakeup.channel,
+    eventChannel: methodChannelLocationWakeup.eventChannel,
+    methodHandler: (methodCall) async {
+      if (methodCall.method == 'startMonitoring') {
+        receivedStartMonitoring = true;
+      }
+      return null;
+    },
+  );
 
-    //Create the Plugin
-    final locationWakeup = LocationWakeup();
+  //Create the Plugin
+  final locationWakeup = LocationWakeup();
 
-    //Initialize the plugin
-    await locationWakeup.startMonitoring();
+  //Initialize the plugin
+  await locationWakeup.startMonitoring();
 
-    final locationData = <String, dynamic>{
-      'latitude': 40.7128,
-      'longitude': -74.0060,
-      'altitude': 500.0,
-      'speed': 5.0,
-      'timestamp': 1677648652.0,
-      'permissionStatus': 'granted',
-    };
+  final locationData = <String, dynamic>{
+    'latitude': 40.7128,
+    'longitude': -74.0060,
+    'altitude': 500.0,
+    'speed': 5.0,
+    'timestamp': 1677648652.0,
+    'permissionStatus': 'granted',
+  };
 
-    //Send the event to the EventChannel (Mimics the Swift code)
-    //eventSink?(locationData)
-    await sendToEventChannel(locationData);
+  //Send the event to the EventChannel (Mimics the Swift code)
+  //eventSink?(locationData)
+  await sendToEventChannel(locationData);
 
-    //Wait for the first LocationResult on the stream
-    final locationResult = await locationWakeup.locationUpdates.first;
+  //Wait for the first LocationResult on the stream
+  final locationResult = await locationWakeup.locationUpdates.first;
 
-    //Verify that the LocationResult is correct
-    expect(receivedStartMonitoring, isTrue);
-    expect(locationResult.locationOrEmpty.latitude, locationData['latitude']);
-    expect(locationResult.locationOrEmpty.longitude, locationData['longitude']);
-  });
+  //Verify that the LocationResult is correct
+  expect(receivedStartMonitoring, isTrue);
+  expect(locationResult.locationOrEmpty.latitude, locationData['latitude']);
+  expect(locationResult.locationOrEmpty.longitude, locationData['longitude']);
+});
 }
