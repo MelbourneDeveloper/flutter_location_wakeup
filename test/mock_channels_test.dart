d06c4cd3950db6523f4bb31fcc2a2306d7a843b6
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2023-09-23 21:48:50 +1000

    Break out the function

--- test/mock_channels_test.dart
+++ test/mock_channels_test.dart
@@ -11,22 +11,6 @@ void main() {
     final methodChannelLocationWakeup =
         LocationWakeupPlatform.instance as MethodChannelLocationWakeup;
 
-    final plugin = LocationWakeup();
-
-    tester.binding.defaultBinaryMessenger.setMockMethodCallHandler(
-      methodChannelLocationWakeup.channel,
-      (methodCall) async {
-        if (methodCall.method == 'startMonitoring') {
-          receivedStartMonitoring = true;
-        }
-        return null;
-      },
-    );
-
-    TestWidgetsFlutterBinding.ensureInitialized();
-
-    await plugin.startMonitoring();
-
     final locationData = <String, dynamic>{
       'latitude': 40.7128,
       'longitude': -74.0060,
@@ -36,24 +20,62 @@ void main() {
       'permissionStatus': 'granted',
     };
 
-    final methodCall = MethodCall('listen', locationData);
-
-    final encodedData =
-        const StandardMethodCodec().encodeMethodCall(methodCall);
-
-    final fakeStreamHandler = FakeStreamHandler();
-    tester.binding.defaultBinaryMessenger.setMockStreamHandler(
+    final locationResult =
+        await initializeAndEmitOne<LocationWakeup, LocationResult>(
+      LocationWakeup(),
+      tester,
+      methodChannelLocationWakeup.channel,
       methodChannelLocationWakeup.eventChannel,
-      fakeStreamHandler,
+      (p) => p.startMonitoring(),
+      (methodCall) async {
+        if (methodCall.method == 'startMonitoring') {
+          receivedStartMonitoring = true;
+        }
+        return null;
+      },
+      locationData,
+      (p) => p.locationUpdates.first,
     );
-
-    await methodChannelLocationWakeup.eventChannel.binaryMessenger
-        .send('loc_stream', encodedData);
-
-    final locationResult = await plugin.locationUpdates.first;
 
     expect(receivedStartMonitoring, isTrue);
     expect(locationResult.locationOrEmpty.latitude, locationData['latitude']);
     expect(locationResult.locationOrEmpty.longitude, locationData['longitude']);
   });
+}
+
+///Initializes the plugin, ensures that the initialization
+///reaches the platform side, and emits one event from 
+///the event channel.
+Future<R> initializeAndEmitOne<TPlugin, R>(
+  TPlugin plugin,
+  WidgetTester tester,
+  MethodChannel methodChannel,
+  EventChannel eventChannel,
+  Future<void> Function(TPlugin plugin) initializePlugin,
+  Future<Object?>? Function(MethodCall)? handler,
+  Map<String, dynamic> firstStreamData,
+  Future<R> Function(TPlugin plugin) onEvent,
+) async {
+  tester.binding.defaultBinaryMessenger.setMockMethodCallHandler(
+    methodChannel,
+    handler,
+  );
+
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  await initializePlugin(plugin);
+
+  final methodCall = MethodCall('listen', firstStreamData);
+
+  final encodedData = const StandardMethodCodec().encodeMethodCall(methodCall);
+
+  final fakeStreamHandler = FakeStreamHandler();
+  tester.binding.defaultBinaryMessenger.setMockStreamHandler(
+    eventChannel,
+    fakeStreamHandler,
+  );
+
+  await eventChannel.binaryMessenger.send(eventChannel.name, encodedData);
+
+  return onEvent(plugin);
 }
